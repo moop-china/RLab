ea7336161197433a20a7487a33ee5fedb9991f0d
shark_wang <longwind.wang@gmail.com>
2019-03-02 19:38:29 +0800

    init

---
+++ binder/install.R
@@ -0,0 +1,97 @@
+### install regular packages
+
+install.packages("BH") # python support in RMarkdown
+install.packages("C50") # python support in RMarkdown
+install.packages("DBI") # python support in RMarkdown
+install.packages("Formula") # python support in RMarkdown
+install.packages("Hmisc") # python support in RMarkdown
+install.packages("LaF") # python support in RMarkdown
+install.packages("R6") # python support in RMarkdown
+install.packages("RColorBrewer") # python support in RMarkdown
+install.packages("RJDBC") # python support in RMarkdown
+install.packages("RJSONIO") # python support in RMarkdown
+install.packages("Rcpp") # python support in RMarkdown
+install.packages("acepack") # python support in RMarkdown
+install.packages("amap") # python support in RMarkdown
+install.packages("chron") # python support in RMarkdown
+install.packages("colorspace") # python support in RMarkdown
+install.packages("curl") # python support in RMarkdown
+install.packages("data.table") # python support in RMarkdown
+install.packages("devtools") # python support in RMarkdown
+install.packages("dichromat") # python support in RMarkdown
+install.packages("digest") # python support in RMarkdown
+install.packages("docopt") # python support in RMarkdown
+install.packages("dplyr") # python support in RMarkdown
+install.packages("ggplot2") # python support in RMarkdown
+install.packages("git2r") # python support in RMarkdown
+install.packages("gridExtra") # python support in RMarkdown
+install.packages("gtable") # python support in RMarkdown
+install.packages("httr") # python support in RMarkdown
+install.packages("jsonlite") # python support in RMarkdown
+install.packages("labeling") # python support in RMarkdown
+install.packages("latticeExtra") # python support in RMarkdown
+install.packages("lazyeval") # python support in RMarkdown
+install.packages("lubridate") # python support in RMarkdown
+install.packages("magrittr") # python support in RMarkdown
+install.packages("markdown") # python support in RMarkdown
+install.packages("memoise") # python support in RMarkdown
+install.packages("mice") # python support in RMarkdown
+install.packages("mime") # python support in RMarkdown
+install.packages("munsell") # python support in RMarkdown
+install.packages("openssl") # python support in RMarkdown
+install.packages("partykit") # python support in RMarkdown
+install.packages("pastecs") # python support in RMarkdown
+install.packages("plyr") # python support in RMarkdown
+install.packages("rJava") # python support in RMarkdown
+install.packages("reshape2") # python support in RMarkdown
+install.packages("rjson") # python support in RMarkdown
+install.packages("rmongodb") # python support in RMarkdown
+install.packages("rredis") # python support in RMarkdown
+install.packages("rstudioapi") # python support in RMarkdown
+install.packages("scales") # python support in RMarkdown
+install.packages("stringi") # python support in RMarkdown
+install.packages("stringr") # python support in RMarkdown
+install.packages("tibble") # python support in RMarkdown
+install.packages("whisker") # python support in RMarkdown
+install.packages("withr") # python support in RMarkdown
+install.packages("xlsx") # python support in RMarkdown
+install.packages("xlsxjars") # python support in RMarkdown
+install.packages("zoo") # python support in RMarkdown
+install.packages("littler") # python support in RMarkdown
+install.packages("KernSmooth") # python support in RMarkdown
+install.packages("MASS") # python support in RMarkdown
+install.packages("Matrix") # python support in RMarkdown
+install.packages("base") # python support in RMarkdown
+install.packages("boot") # python support in RMarkdown
+install.packages("class") # python support in RMarkdown
+install.packages("cluster") # python support in RMarkdown
+install.packages("codetools") # python support in RMarkdown
+install.packages("compiler") # python support in RMarkdown
+install.packages("datasets") # python support in RMarkdown
+install.packages("foreign") # python support in RMarkdown
+install.packages("grDevices") # python support in RMarkdown
+install.packages("graphics") # python support in RMarkdown
+install.packages("grid") # python support in RMarkdown
+install.packages("lattice") # python support in RMarkdown
+install.packages("methods") # python support in RMarkdown
+install.packages("mgcv") # python support in RMarkdown
+install.packages("nlme") # python support in RMarkdown
+install.packages("nnet") # python support in RMarkdown
+install.packages("parallel") # python support in RMarkdown
+install.packages("rpart") # python support in RMarkdown
+install.packages("spatial") # python support in RMarkdown
+install.packages("stats") # python support in RMarkdown
+install.packages("stats") # python support in RMarkdown
+install.packages("survival") # python support in RMarkdown
+install.packages("tcltk") # python support in RMarkdown
+install.packages("tools") # python support in RMarkdown
+install.packages("class") # python support in RMarkdown
+install.packages("utils") # python support in RMarkdown
+
+### install bioconductor packages
+# install.packages("BiocManager")
+# BiocManager::install("package")
+
+### install GitHub packages (tag = commit, branch or release tag)
+# install.packages("devtools")
+# devtools::install_github("user/repo", ref = "tag")
